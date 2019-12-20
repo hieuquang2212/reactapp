fa954cf292cfdd0c4f538a7bb2b872c5396e7d14
HIEUQUANG <hieuquang2212>
2019-12-20 19:37:36 +0700

    Updates

---
+++ precache-manifest.aebc732292f33bce851bb80f573e88d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f94736abe5c9adf1c7805bdf071f7a",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "42ff9cad2c49dfc1ac36",
+    "url": "/reactapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/reactapp/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactapp/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42ff9cad2c49dfc1ac36",
+    "url": "/reactapp/static/js/main.510340e3.chunk.js"
+  },
+  {
+    "revision": "5bcba4f5d97e260406b4",
+    "url": "/reactapp/static/js/runtime-main.51ee770a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
